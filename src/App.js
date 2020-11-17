556ee8ac9eb6c69bb2cfd9f51d1d5785cf314f43
jabergma <jabergma@kth.se>
2020-11-17 23:42:29 +0100

    login

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-import { useAuthState } from "react-firebase-hooks";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 firebase.initializeApp({
@@ -22,12 +22,38 @@ firebase.initializeApp({
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
+
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header></header>
+      <section>
+        {user ? <Test /> : <SignIn />}
+      </section>
     </div>
   );
 }
+
+function SignIn(){
+  const signInWithGoogle = ()=> {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return(
+    <button onClick={signInWithGoogle}>Sign In With Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={()=> auth.signOut()}>Sign Out</button>
+  )
+}
+
+function Test() {
+  return SignOut();
+}
+
 
 export default App;
---
+++ src/apiConfig.js
@@ -0,0 +1,2 @@
+const BASE_URL="https://www.alphavantage.co/query?";
+const API_KEY="9BK7T37KFC8J0NH2";
