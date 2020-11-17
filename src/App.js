a4b972572382f39db9ac9e1a98d2332d225273c2
jabergma <jabergma@kth.se>
2020-11-17 22:35:37 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBT4r_JoMcQodd1N30Bf6eZnlOmXNgeiR0",
+  authDomain: "project-stockify.firebaseapp.com",
+  databaseURL: "https://project-stockify.firebaseio.com",
+  projectId: "project-stockify",
+  storageBucket: "project-stockify.appspot.com",
+  messagingSenderId: "104577641235",
+  appId: "1:104577641235:web:bcdba796687f59660548e6",
+  measurementId: "G-PLM181PJ08",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
